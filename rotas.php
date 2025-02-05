d837ee5216a4ef52b6de00662f7f3ff2617ce45e
Lucas Paz <lucaspaz283@gmail.com>
2025-02-05 11:35:22 -0300

    routes for url

--- rotas.php
+++ rotas.php
@@ -3,10 +3,10 @@
 use KpEsportes\App\Domain\Router;
 
 require_once __DIR__. "/Router.php";
-Router::add("/home", "index.html");
-Router::add("/admin/login", "TelaLoginKp/index.html");
-Router::add("/auth/verifyemail", "TelaLoginKp/email.html");
-Router::add("/admin/panel", "TelaLoginKp/admin.html");
-Router::add("/show/product", "product.html");
-Router::add("/cart", "cart.html");
-Router::add("/categories", "categories.html");
+Router::add("https://kp-esportes-frontend.vercel.app/home", "index.html");
+Router::add("https://kp-esportes-frontend.vercel.app/admin/login", "TelaLoginKp/index.html");
+Router::add("https://kp-esportes-frontend.vercel.app/auth/verifyemail", "TelaLoginKp/email.html");
+Router::add("https://kp-esportes-frontend.vercel.app/admin/panel", "TelaLoginKp/admin.html");
+Router::add("https://kp-esportes-frontend.vercel.app/show/product", "product.html");
+Router::add("https://kp-esportes-frontend.vercel.app/cart", "cart.html");
+Router::add("https://kp-esportes-frontend.vercel.app/categories", "categories.html");
